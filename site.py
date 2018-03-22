1c3fa02c8d51da961364ca239ad2bae8b4f3ffa3
Killy85 <goureau.n.a@hotmail.fr>
2018-03-22 16:13:15 +0100

    Update files structure

--- site.py
+++ site.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 import os
+import json as JSON
 
 root_dir = os.path.dirname(os.getcwd())
 
@@ -23,10 +24,40 @@ def ipcount():
 def send_local_bl():
     bl_list = ["192.168.1.1", "192.168.1.2","192.168.1.3"]
     with open('./tmp/bl.txt', 'w+')as f:
-        f.write("\n".join(bl_list))
+        f.write("\n".join(bl_list, "\n"))
     return 'oui'
-    
 
+@app.route('/get_commune_black_list')    
+def get_commune_black_list():
+    with open('./tmp/commune_bl.json', 'r') as json_data:
+        commun_bl = JSON.load(json_data)
+    return "\n".join(commun_bl['blIP'])
+
+@app.route('/update_whitelist')
+def update_whitelist():
+    oui = open('./tmp/nginx.conf').readlines()
+    oui_render = reduce( lambda x ,y : x + y , oui)
+    return render_template('form-editor.html', placeholder = oui_render)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return render_template('400.html'), 400
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(503)
+def page_not_found(e):
+    return render_template('503.html'), 503
 
 if __name__ == '__main__':
     app.run()
