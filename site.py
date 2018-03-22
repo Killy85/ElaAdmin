35f089514802890af25c968478441c6b31ea566e
Killy85 <goureau.n.a@hotmail.fr>
2018-03-22 14:02:26 +0100

    Adding routes skeletton

--- site.py
+++ site.py
@@ -15,5 +15,17 @@ def index():
 def analytics():
     return render_template('index1.html')
 
+@app.route('/ipcount')
+def ipcount():
+    return '12'
+
+@app.route('/send_local_black_list')
+def send_local_bl():
+    bl_list = ["192.168.1.1", "192.168.1.2","192.168.1.3"]
+    with open('./tmp/bl.txt', 'w+')as f:
+        return f.write(bl_list)
+    
+
+
 if __name__ == '__main__':
     app.run()
